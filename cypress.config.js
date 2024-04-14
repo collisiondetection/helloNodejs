11864cddce6ba2d41c23449f25359ec4cee5dc23
Dan <info@collisiondetection.biz>
2024-04-14 20:08:30 +0700

    support off

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,22 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl: "http://localhost:3000",
-    viewportWidth: 1280,
-    viewportHeight: 720,
-    supportFile: false,
-    defaultCommandTimeout: 5000,
-    reporter: "mochawesome",
-    reporterOptions: {
-      reportDir: "cypress/reports",
-      overwrite: false,
-      html: false,
-      json: true
-    },
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  }
+    e2e: {
+        baseUrl: "http://localhost:3000",
+        viewportWidth: 1280,
+        viewportHeight: 720,
+        defaultCommandTimeout: 5000,
+        reporter: "mochawesome",
+        reporterOptions: {
+            reportDir: "cypress/reports",
+            overwrite: false,
+            html: false,
+            json: true
+        },
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        supportFile: false // Set supportFile to false
+    }
 });
+
